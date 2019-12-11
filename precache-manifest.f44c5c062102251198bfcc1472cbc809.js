ba65b3c01c87ab8e6c60b4e29eb6be1be9a3dde0
sma2lbao <sma2lbao@users.noreply.github.com>
2019-12-11 10:21:26 +0000

    Automated deployment: Wed Dec 11 10:21:26 UTC 2019 ead3cb3153d08201a870b58d78e0ca8c7a4c09db

---
+++ precache-manifest.f44c5c062102251198bfcc1472cbc809.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5fd9d4c0c3d87c5c7b38d951f8b2069",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90cd10ce983229aa99c3",
+    "url": "/static/js/2.ae1fa00d.chunk.js"
+  },
+  {
+    "revision": "65b6c48ef7617db47f9c",
+    "url": "/static/js/main.779e87fb.chunk.js"
+  },
+  {
+    "revision": "20d7616e390d3d06064f",
+    "url": "/static/js/runtime-main.892c0e6c.js"
+  }
+]);
